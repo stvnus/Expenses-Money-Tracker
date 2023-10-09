f0b9c98a4fa03b09dd9b76e577d1eb80bd18fc83
stvnus <stvnus98@gmail.com>
2023-10-09 19:05:36 +0700

    feat : add feature download pdf transaction and styling color button

--- src/App.jsx
+++ src/App.jsx
@@ -1,168 +1,247 @@
-import './App.css';
-import React from 'react';
-import ModalCreate from './component/ModalCreate';
+import "./App.css";
+import React from "react";
+import ModalCreate from "./component/ModalCreate";
+import jsPDF from "jspdf";
 
 class App extends React.Component {
   constructor() {
     super();
-    
+
     this.state = {
-      sisaUang : 0,
-      persentaseUang : 0, 
-      pemasukanUang : 0, 
-      pengeluaranUang : 0, 
-      transaksiIN : 0, 
-      transaksiOUT : 0, 
-      summary : [
-       
-      ]
-    }
+      sisaUang: 0,
+      persentaseUang: 0,
+      pemasukanUang: 0,
+      pengeluaranUang: 0,
+      transaksiIN: 0,
+      transaksiOUT: 0,
+      summary: [],
+    };
 
     this.tambahItem = this.tambahItem.bind(this);
     this.fnHitung = this.fnHitung.bind(this);
   }
 
-  tambahItem(objek){
-    let newData = [...this.state.summary, objek]
-    let dataUangIN = newData.filter( (item)=> item.category === 'IN' );
-    let nominalUang = dataUangIN.map((item)=> item.nominal );
-    let jumlahUangIn = nominalUang.reduce((total, num)=> total + num, 0 ); 
+  tambahItem(objek) {
+    let newData = [...this.state.summary, objek];
+    let dataUangIN = newData.filter((item) => item.category === "IN");
+    let nominalUang = dataUangIN.map((item) => item.nominal);
+    let jumlahUangIn = nominalUang.reduce((total, num) => total + num, 0);
 
-    let dataUangOUT = newData.filter( (item)=> item.category === 'OUT' );
-    let nominalUangOUT = dataUangOUT.map((item)=> item.nominal );
-    let jumlahUangOUT = nominalUangOUT.reduce((total, num)=> total + num, 0 )
+    let dataUangOUT = newData.filter((item) => item.category === "OUT");
+    let nominalUangOUT = dataUangOUT.map((item) => item.nominal);
+    let jumlahUangOUT = nominalUangOUT.reduce((total, num) => total + num, 0);
 
     this.setState({
-      pemasukanUang : jumlahUangIn, 
-      transaksiIN : nominalUang.length, 
-      pengeluaranUang : jumlahUangOUT,
-      transaksiOUT : nominalUangOUT.length, 
-      sisaUang : jumlahUangIn - jumlahUangOUT,
-      persentaseUang : (jumlahUangIn - jumlahUangOUT)/jumlahUangIn * 100, 
-      summary : newData
-    })
+      pemasukanUang: jumlahUangIn,
+      transaksiIN: nominalUang.length,
+      pengeluaranUang: jumlahUangOUT,
+      transaksiOUT: nominalUangOUT.length,
+      sisaUang: jumlahUangIn - jumlahUangOUT,
+      persentaseUang: ((jumlahUangIn - jumlahUangOUT) / jumlahUangIn) * 100,
+      summary: newData,
+    });
   }
 
   fnHitung() {
-    let dataUangIN = this.state.summary.filter( (item)=> item.category === 'IN' );
-    let nominalUang = dataUangIN.map((item)=> item.nominal );
-    let jumlahUangIn = nominalUang.reduce((total, num)=> total + num ); 
-    let dataUangOUT = this.state.summary.filter( (item)=> item.category === 'OUT' );
-    let nominalUangOUT = dataUangOUT.map((item)=> item.nominal );
-    let jumlahUangOUT = nominalUangOUT.reduce((total, num)=> total + num )
+    let dataUangIN = this.state.summary.filter(
+      (item) => item.category === "IN"
+    );
+    let nominalUang = dataUangIN.map((item) => item.nominal);
+    let jumlahUangIn = nominalUang.reduce((total, num) => total + num);
+
+    let dataUangOUT = this.state.summary.filter(
+      (item) => item.category === "OUT"
+    );
+    let nominalUangOUT = dataUangOUT.map((item) => item.nominal);
+    let jumlahUangOUT = nominalUangOUT.reduce((total, num) => total + num);
 
     this.setState({
-      pemasukanUang : jumlahUangIn, 
-      transaksiIN : nominalUang.length, 
-      pengeluaranUang : jumlahUangOUT,
-      transaksiOUT : nominalUangOUT.length, 
-      sisaUang : jumlahUangIn - jumlahUangOUT,
-      persentaseUang : (jumlahUangIn - jumlahUangOUT)/jumlahUangIn * 100
-    })
+      pemasukanUang: jumlahUangIn,
+      transaksiIN: nominalUang.length,
+      pengeluaranUang: jumlahUangOUT,
+      transaksiOUT: nominalUangOUT.length,
+      sisaUang: jumlahUangIn - jumlahUangOUT,
+      persentaseUang: ((jumlahUangIn - jumlahUangOUT) / jumlahUangIn) * 100,
+    });
   }
 
   componentDidMount() {
-    if(this.state.summary.length < 1 ) {
-      console.log('ok')
+    if (this.state.summary.length < 1) {
+      console.log("ok");
     } else {
-      this.fnHitung()
+      this.fnHitung();
     }
   }
 
+  generatePDF() {
+    const { summary, sisaUang } = this.state;
+    const doc = new jsPDF();
+    
+    doc.text("Ringkasan Transaksi", 10, 10);
+    
+    let yPos = 30;
+    summary.forEach((sum) => {
+      yPos += 10;
+      doc.text(sum.deskripsi, 10, yPos);
+      doc.text(sum.tanggal, 80, yPos);
+      
+      // Tambahkan tanda minus (-) jika itu adalah pengeluaran
+      const nominal = sum.category === "OUT" ? `-Rp. ${Math.abs(sum.nominal)}` : `Rp. ${sum.nominal}`;
+      doc.text(nominal, 150, yPos);
+    });
+    
+  
+    yPos += 20;
+    doc.text(`Sisa Uang: Rp. ${sisaUang}`, 10, yPos);
+    
+    doc.save("ringkasan_transaksi.pdf");
+  }
+  
 
-  render(){
+  render() {
     return (
       <>
-        <div className='container py-5'>
-          <div className='row'>
-            <div className='col-12 text-center'>
-              <h1 className='fw-bold'>Money Tracker Lite</h1>
-              <hr className='w-75 mx-auto' />
-              <h2 className='fw-bold'>Rp. {this.state.sisaUang} ,-</h2>
-              <span className='title-md'>Sisa uang kamu tersisa {this.state.persentaseUang}% lagi</span>
+        <div className="container py-5">
+          <div className="row">
+            <div className="col-12 text-center">
+              <h1 className="fw-bold">Money Tracker Lite</h1>
+              <hr className="w-75 mx-auto" />
+              <h2 className="fw-bold">Rp. {this.state.sisaUang} </h2>
+              <span className="title-sm">
+                Sisa uang kamu tersisa {this.state.persentaseUang}% lagi
+              </span>
             </div>
           </div>
 
-          <div className='row mt-4'>
-            <div className='col-6'>
-              <div className='card-wraper p-4'>
-                <div className='icon-wrapper-IN mb-1'>
-                  <i className="bi bi-wallet2"></i>
+          <div className="row mt-4">
+            <div className="col-6">
+              <div className="card-wraper p-4 card-bg-pemasukan">
+                <div className="d-flex align-items-center mb-1">
+                  <i className="bi bi-wallet2 mr-2"></i>
+                  <span className="title-md">Pemasukan</span>
                 </div>
-                <span className='title-sm'>Pemasukan</span>
-                <h3 className='fw-bold'>Rp. {this.state.pemasukanUang},-</h3>
+                <h3 className="fw-bold">Rp. {this.state.pemasukanUang},-</h3>
                 <div>
-                  <span className='title-sm text-ungu fw-bold'>{this.state.transaksiIN}</span><span className='title-sm'> Transaksi</span>
+                  <span className="title-sm text-ungu fw-bold">
+                    {this.state.transaksiIN}
+                  </span>
+                  <span className="title-sm"> Transaksi</span>
                 </div>
               </div>
             </div>
 
-            <div className='col-6'>
-              <div className='card-wraper p-4'>
-                <div className='icon-wrapper-OUT mb-1'>
-                  <i className="bi bi-cash-stack"></i>
+            <div className="col-6">
+              <div className="card-wraper p-4 card-bg-pengeluaran">
+                <div className="d-flex align-items-center mb-1">
+                  <i className="bi bi-cash-stack mr-2"></i>
+                  <span className="title-md">Pengeluaran</span>
                 </div>
-                <span className='title-sm'>Pengeluaran</span>
-                <h3 className='fw-bold'>Rp. {this.state.pengeluaranUang},-</h3>
+                <h3 className="fw-bold">
+                  Rp. {this.state.pengeluaranUang >= 0 ? this.state.pengeluaranUang : Math.abs(this.state.pengeluaranUang)}
+                </h3>
                 <div>
-                  <span className='title-sm text-ungu fw-bold'>{this.state.transaksiOUT}</span><span className='title-sm'> Transaksi</span>
+                  <span className="title-sm text-ungu fw-bold">
+                    {this.state.transaksiOUT}
+                  </span>
+                  <span className="title-sm"> Transaksi</span>
                 </div>
               </div>
             </div>
-
           </div>
 
-          <div className='row mt-5'>
-            <div className='col-12 d-flex justify-content-between align-items-center'>
+          <div className="row mt-5">
+            <div className="col-12 d-flex justify-content-between align-items-center">
               <h4>Ringkasan Transaksi</h4>
-              <div className='wrapper-button d-flex'>
-                <ModalCreate action={this.tambahItem} category="IN" variant="button btn-ungu px-3 py-2 me-2" text="Pemasukan" icon="bi bi-plus-circle-fill" modalheading="Tambahkan Pemasukan"/>
-                <ModalCreate action={this.tambahItem} category="OUT" variant="button btn-pink px-3 py-2" text="Pengeluaran" icon="bi bi-dash-circle-fill" modalheading="Tambahkan Pengeluaran"/>
+
+              <div className="wrapper-button d-flex">
+                <button
+                  className="btn btn-secondary me-2"
+                  onClick={() => this.generatePDF()}
+                >
+                  <i className="bi bi-file-earmark-arrow-down-fill me-1"></i>{" "}
+                  Transaksi
+                </button>
+                <ModalCreate
+                  action={this.tambahItem}
+                  category="IN"
+                  variant="button btn-pemasukkan px-3 py-2 me-2"
+                  text={
+                    <>
+                      <i className="bi bi-patch-plus-fill me-1"></i> Pemasukan
+                    </>
+                  }
+                  modalheading="Tambahkan Pemasukan"
+                />
+                <ModalCreate
+                  action={this.tambahItem}
+                  category="OUT"
+                  variant="button btn-pink px-3 py-2"
+                  text={
+                    <>
+                      <i className="bi bi-cart-dash me-1"></i> Pengeluaran
+                    </>
+                  }
+                  modalheading="Tambahkan Pengeluaran"
+                />
               </div>
             </div>
           </div>
 
-          <div className='row mt-4'>
-            { this.state.summary.length < 1 && <Alert /> }
-            { this.state.summary.map((sum, index)=> {
+          <div className="row mt-4">
+            {this.state.summary.length < 1 && <Alert />}
+            {this.state.summary.map((sum, index) => {
               return (
-                <div key={index} className='mb-3 col-12 d-flex justify-content-between align-items-center'>
-                  <div className='d-flex align-items-center'>
-                    <div className={sum.category === 'IN' ? 'icon-wrapper-IN' : 'icon-wrapper-OUT'}>
-                      <i className={sum.category === 'IN' ? 'bi bi-wallet2' : 'bi bi-bag-dash'}></i>
+                <div
+                  key={index}
+                  className="mb-3 col-12 d-flex justify-content-between align-items-center"
+                >
+                  <div className="d-flex align-items-center">
+                    <div
+                      className={
+                        sum.category === "IN"
+                          ? "icon-wrapper-IN"
+                          : "icon-wrapper-OUT"
+                      }
+                    >
+                      <i
+                        className={
+                          sum.category === "IN"
+                            ? "bi bi-wallet2"
+                            : "bi bi-bag-dash"
+                        }
+                      ></i>
                     </div>
-                    <div className='transaction ms-3 d-flex flex-column'>
+                    <div className="transaction ms-3 d-flex flex-column">
                       <h6>{sum.deskripsi}</h6>
-                      <span className='title-sm'>{sum.tanggal}</span>
+                      <span className="title-sm">{sum.tanggal}</span>
                     </div>
                   </div>
-    
-                  <h5 className={sum.category === 'IN' ? 'text-money-in' : 'text-money-out'}>Rp. {sum.nominal}</h5>
+
+                  <h5
+                    className={
+                      sum.category === "IN" ? "text-money-in" : "text-money-out"
+                    }
+                  >
+                    {sum.category === "IN" ? `Rp. ${sum.nominal}` : `-Rp. ${Math.abs(sum.nominal)}`}
+                  </h5>
                 </div>
-              )
-            }) }
+              );
+            })}
           </div>
         </div>
       </>
-    )
+    );
   }
 }
 
-class Alert extends React.Component{
-
+class Alert extends React.Component {
   constructor() {
-    super()
+    super();
   }
 
-  render(){
-    return (
-      <h1>Data Masih Kosong</h1>
-    )
-  } 
-
+  render() {
+    return <h1>Data Masih Kosong</h1>;
+  }
 }
-
-
-
 
 export default App;
--- src/component/ModalCreate.jsx
+++ src/component/ModalCreate.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types"; // Import PropTypes
+import PropTypes from "prop-types";
 import Modal from 'react-bootstrap/Modal';
 
 class ModalCreate extends React.Component {
@@ -10,7 +10,10 @@ class ModalCreate extends React.Component {
             deskripsi: '',
             nominal: 0,
             tanggal: '',
-            category: ''
+            category: '',
+            deskripsiError: '',
+            nominalError: '',
+            tanggalError: '' // Tambahkan state tanggalError
         }
 
         this.handleClose = this.handleClose.bind(this);
@@ -33,26 +36,76 @@ class ModalCreate extends React.Component {
     }
 
     handleChange(evt) {
-        this.setState({
-            [evt.target.name]: evt.target.value
-        })
+        if (evt.target.name === 'nominal') {
+            const numericValue = parseInt(evt.target.value);
+            if (isNaN(numericValue)) {
+                this.setState({
+                    nominalError: 'Harap input angka',
+                    nominal: evt.target.value
+                });
+            } else {
+                this.setState({
+                    nominalError: '',
+                    nominal: numericValue
+                });
+            }
+        } else if (evt.target.name === 'deskripsi') {
+            this.setState({
+                deskripsi: evt.target.value,
+                deskripsiError: evt.target.value.trim() === '' ? 'Deskripsi tidak boleh kosong' : ''
+            });
+        } else {
+            this.setState({
+                [evt.target.name]: evt.target.value
+            });
+        }
     }
 
     tambahItem() {
-        const Data = {
-            deskripsi: this.state.deskripsi,
-            nominal: parseInt(this.state.nominal),
-            tanggal: this.state.tanggal,
-            category: this.state.category
+        if (this.state.deskripsi.trim() === '') {
+            this.setState({
+                deskripsiError: 'Deskripsi tidak boleh kosong'
+            });
+        } else if (!this.state.tanggal) { // Periksa apakah tanggal kosong
+            this.setState({
+                tanggalError: 'Tanggal tidak boleh kosong'
+            });
+        } else {
+            this.setState({
+                deskripsiError: '',
+                tanggalError: '' // Reset pesan error tanggal jika tanggal diisi
+            });
+
+            if (this.state.nominalError === '') {
+                if (this.state.nominal < 500) {
+                    this.setState({
+                        nominalError: 'Minimal pengeluaran 500 rupiah'
+                    });
+                } else {
+                    this.setState({
+                        nominalError: ''
+                    });
+
+                    const Data = {
+                        deskripsi: this.state.deskripsi,
+                        nominal: this.state.nominal,
+                        tanggal: this.state.tanggal,
+                        category: this.state.category
+                    }
+                    const fnTambahItem = this.props.action;
+                    fnTambahItem(Data);
+                    this.setState({
+                        show: false,
+                        deskripsi: '',
+                        nominal: 0,
+                        tanggal: '',
+                        deskripsiError: '',
+                        nominalError: '',
+                        tanggalError: '' // Reset pesan error tanggal saat berhasil menambahkan item
+                    });
+                }
+            }
         }
-        const fnTambahItem = this.props.action;
-        fnTambahItem(Data);
-        this.setState({
-            show: false,
-            deskripsi: '',
-            nominal: 0,
-            tanggal: '',
-        })
     }
 
     render() {
@@ -69,36 +122,39 @@ class ModalCreate extends React.Component {
                             <label className="form-label">Deskripsi</label>
                             <input
                                 type="text"
-                                className="form-control"
+                                className={`form-control ${this.state.deskripsiError ? 'is-invalid' : ''}`}
                                 placeholder="Masukan deskripsi"
                                 name='deskripsi'
                                 value={this.state.deskripsi}
                                 onChange={this.handleChange}
                             />
+                            {this.state.deskripsiError && <div className="invalid-feedback">{this.state.deskripsiError}</div>}
                         </div>
 
                         <div className="mb-3">
                             <label className="form-label">Nominal</label>
                             <input
-                                type="number"
-                                className="form-control"
-                                placeholder="Masukan deskripsi"
+                                type="text"
+                                className={`form-control ${this.state.nominalError ? 'is-invalid' : ''}`}
+                                placeholder="Masukan Nominal"
                                 name='nominal'
                                 value={this.state.nominal}
                                 onChange={this.handleChange}
                             />
+                            {this.state.nominalError && <div className="invalid-feedback">{this.state.nominalError}</div>}
                         </div>
 
                         <div className="mb-3">
                             <label className="form-label">Tanggal</label>
                             <input
                                 type="date"
-                                className="form-control"
-                                placeholder="Masukan deskripsi"
+                                className={`form-control ${this.state.tanggalError ? 'is-invalid' : ''}`}
+                                placeholder="Masukan tanggal"
                                 name='tanggal'
                                 value={this.state.tanggal}
                                 onChange={this.handleChange}
                             />
+                            {this.state.tanggalError && <div className="invalid-feedback">{this.state.tanggalError}</div>}
                         </div>
 
                         <div>
@@ -123,7 +179,6 @@ class ModalCreate extends React.Component {
     }
 }
 
-// Add PropTypes validation for the props used in the component
 ModalCreate.propTypes = {
     variant: PropTypes.string.isRequired,
     text: PropTypes.string.isRequired,
